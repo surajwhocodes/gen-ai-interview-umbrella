458cfac655a53785e6fbf46637fa4d33734cd731
surajdpatel <surajdpatel95@outlook.com>
2026-06-19 17:10:07 +0530

    puppeteer updates

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,16 @@
 import { RouterProvider } from "react-router"
 import { router } from "./app.routes.jsx"
+import { AuthProvider } from "./features/auth/auth.context.jsx"
+import { InterviewProvider } from "./features/interview/interview.context.jsx"
 
 function App() {
 
   return (
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <InterviewProvider>
+        <RouterProvider router={router} />
+      </InterviewProvider>
+    </AuthProvider>
   )
 }
 
--- frontend/src/features/auth/pages/Register.jsx
+++ frontend/src/features/auth/pages/Register.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
-import { useNavigate,Link } from 'react-router'
+import React,{useState} from 'react'
+import { useNavigate, Link } from 'react-router'
+import { useAuth } from '../hooks/useAuth'
 
 const Register = () => {
 
     const navigate = useNavigate()
+    const [ username, setUsername ] = useState("")
+    const [ email, setEmail ] = useState("")
+    const [ password, setPassword ] = useState("")
 
-    const handleSubmit = (e) => {
+    const {loading,handleRegister} = useAuth()
+    
+    const handleSubmit = async (e) => {
         e.preventDefault()
+        await handleRegister({username,email,password})
+        navigate("/")
+    }
+
+    if(loading){
+        return (<main><h1>Loading.......</h1></main>)
     }
 
     return (
@@ -18,15 +30,21 @@ const Register = () => {
 
                     <div className="input-group">
                         <label htmlFor="username">Username</label>
-                        <input type="text" id="username" name='username' placeholder='Enter username' />
+                        <input
+                            onChange={(e) => { setUsername(e.target.value) }}
+                            type="text" id="username" name='username' placeholder='Enter username' />
                     </div>
                     <div className="input-group">
                         <label htmlFor="email">Email</label>
-                        <input type="email" id="email" name='email' placeholder='Enter email address' />
+                        <input
+                            onChange={(e) => { setEmail(e.target.value) }}
+                            type="email" id="email" name='email' placeholder='Enter email address' />
                     </div>
                     <div className="input-group">
                         <label htmlFor="password">Password</label>
-                        <input type="password" id="password" name='password' placeholder='Enter password' />
+                        <input
+                            onChange={(e) => { setPassword(e.target.value) }}
+                            type="password" id="password" name='password' placeholder='Enter password' />
                     </div>
 
                     <button className='button primary-button' >Register</button>
